4bbbc916b27ba129263f5a9370ac0bcfe1d2dd53
Scott Stafford <sas4z@yahoo.com>
2016-01-12 17:22:57 -0500

    Create Powerpoint file from PresentationML example under src/main/resources/powerpoint

--- src/main/java/com/marklogic/presentationml/Application.java
+++ src/main/java/com/marklogic/presentationml/Application.java
@@ -1,9 +1,16 @@
 package com.marklogic.presentationml;
 
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -15,17 +22,9 @@ public class Application {
 	public static void main(String[] args) {
 		
 		try {
-			
-			FileOutputStream fos = new FileOutputStream("example.pptx");
-			ZipOutputStream zos = new ZipOutputStream(fos);
-		
-			Resource res = new ClassPathResource("powerpoint/[Content_Types].xml");
-			addToZipFile(res, zos);
-
-	
-			zos.close();
-			fos.close();
-	
+			File out = new File("example.pptx");
+			Resource dir = new ClassPathResource("powerpoint/");
+			zip(dir.getFile(), out);	
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -33,24 +32,65 @@ public class Application {
 		}
 		
 	}
+	
+	@SuppressWarnings("resource")
+	public static void zip(File dir, File zipFile) throws IOException {
+	    URI base = dir.toURI();
+	    Deque<File> queue = new LinkedList<File>();
+	    queue.push(dir);
+	    OutputStream out = new FileOutputStream(zipFile);
+	    Closeable res = out;
+	    try {
+	      ZipOutputStream zout = new ZipOutputStream(out);
+	      res = zout;
+	      while (!queue.isEmpty()) {
+	        dir = queue.pop();
+	        for (File kid : dir.listFiles()) {
+	          String name = base.relativize(kid.toURI()).getPath();
+	          if (kid.isDirectory()) {
+	            queue.push(kid);
+	            name = name.endsWith("/") ? name : name + "/";
+	            zout.putNextEntry(new ZipEntry(name));
+	          } else {
+	            zout.putNextEntry(new ZipEntry(name));
+	            copy(kid, zout);
+	            zout.closeEntry();
+	          }
+	        }
+	      }
+	    } finally {
+	      res.close();
+	    }
+	  }
+	
+	private static void copy(InputStream in, OutputStream out) throws IOException {
+	    byte[] buffer = new byte[1024];
+	    while (true) {
+	      int readCount = in.read(buffer);
+	      if (readCount < 0) {
+	        break;
+	      }
+	      out.write(buffer, 0, readCount);
+	    }
+	  }
 
-	public static void addToZipFile(Resource res, ZipOutputStream zos) throws FileNotFoundException, IOException {
-	
-		String fileName = res.getFilename();
-		System.out.println("Writing '" + fileName + "' to zip file");
-		InputStream is = res.getInputStream();
-		ZipEntry zipEntry = new ZipEntry(fileName);
-		zos.putNextEntry(zipEntry);
-	
-		byte[] bytes = new byte[1024];
-		int length;
-		while ((length = is.read(bytes)) >= 0) {
-			zos.write(bytes, 0, length);
-		}
-	
-		zos.closeEntry();
-		is.close();
-	}
+	  private static void copy(File file, OutputStream out) throws IOException {
+	    InputStream in = new FileInputStream(file);
+	    try {
+	      copy(in, out);
+	    } finally {
+	      in.close();
+	    }
+	  }
+
+	  private static void copy(InputStream in, File file) throws IOException {
+	    OutputStream out = new FileOutputStream(file);
+	    try {
+	      copy(in, out);
+	    } finally {
+	      out.close();
+	    }
+	  }
 	
 
 
